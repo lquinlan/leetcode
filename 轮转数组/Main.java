508875966ac7ced06861d4ed900b9f7bf80e8f47
quinlan <quinlantian678@gmail.com>
2023-05-19 21:04:10 +0800

    旋转=》三次翻转

---
+++ 轮转数组/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    void reverse(int[] nums, int l, int r){
+        while(l < r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+        
+    }
+}
