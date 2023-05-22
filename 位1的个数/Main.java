f98c4a5558b93fd18cdd4c74aee39ad8130e3fb2
quinlan <quinlantian678@gmail.com>
2023-05-22 21:59:51 +0800

    新方法O(1)

--- 位1的个数/Main.java
+++ 位1的个数/Main.java
@@ -1,18 +1,28 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
 
 class Solution {
     // you need to treat n as an unsigned value
+    // public int hammingWeight(int n) {
+    // int count = 0;
+    // while (n!= 0) {
+    // n &= (n - 1);
+    // count++;
+    // }
+    // return count;
+
+    // }
     public int hammingWeight(int n) {
-        int count = 0;
-        while (n!= 0) {
-            n &= (n - 1);
-            count++;
-        }
-        return count;
-        
+        n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        n = (n & 0x0f0f0f0f) + ((n >> 4) & 0x0f0f0f0f);
+        n = (n & 0x00ff00ff) + ((n >> 8) & 0x00ff00ff);
+        n = (n & 0x0000ffff) + ((n >> 16) & 0x0000ffff);
+
+        return n;
+
     }
 }
