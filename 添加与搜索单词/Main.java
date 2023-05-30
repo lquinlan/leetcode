0c86390efb05b0c838895bc214b585f9ade96cf4
quinlan <quinlantian678@gmail.com>
2023-05-30 21:36:21 +0800

    字典树+dfs

---
+++ 添加与搜索单词/Main.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
+
+class WordDictionary {
+    TrieNode root;
+    public WordDictionary() {
+        root = new TrieNode();
+
+    }
+    
+    public void addWord(String word) {
+        char[] c = word.toCharArray();
+        TrieNode node = root;
+        for(var i:c){
+            if(node.children[i-'a']==null){
+                node.children[i-'a'] = new TrieNode();
+            }
+            node=node.children[i-'a'];
+        }
+        node.isEnd = true;
+
+    }
+    
+    public boolean search(String word) {
+        return dfs(word,0,root) ;
+
+
+    }
+    boolean dfs(String word,int index,TrieNode root) {
+        if(index==word.length()){
+            return root.isEnd;
+        }
+        char c=word.charAt(index);
+        if(c=='.'){
+            for(int i=0;i<26;++i){
+                if(root.children[i]!=null&&dfs(word,index+1,root.children[i])){
+                    return true;
+                }
+            }
+            return false;
+
+        }else{
+            if(root.children[c-'a']==null){
+                return false;
+            }
+            return dfs(word,index+1,root.children[c-'a']);
+        }
+
+    }
+    class TrieNode {
+        TrieNode[] children;
+        boolean isEnd;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            isEnd = false;
+        }
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
